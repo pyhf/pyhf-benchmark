ed943bf7cfaa762ec947fdb9ba3606cd89c671eb
Bo Zheng <cool_zb@outlook.com>
2020-06-15 12:04:56 -0500

    Add tensorflow support

--- scripts/run.py
+++ scripts/run.py
@@ -89,6 +89,8 @@ def calculate_CLs(bkgonly_json, signal_patch_json):
     )
     if isinstance(pyhf.tensorlib, pyhf.tensor.pytorch_backend):
         return result[0].tolist()[0], result[-1].tolist()
+    elif isinstance(pyhf.tensorlib, pyhf.tensor.tensorflow_backend):
+        return result[0].numpy().tolist()[0], result[-1].numpy().ravel().tolist()
     else:
         return result[0].tolist()[0], result[-1].ravel().tolist()
 
