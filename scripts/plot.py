4f17d0adfd4eee380bdd4d796f44a4b4d56b4689
Bo Zheng <cool_zb@outlook.com>
2020-06-30 18:04:44 -0500

    Save result

--- scripts/plot.py
+++ scripts/plot.py
@@ -14,18 +14,17 @@ def find_latest(directory_name):
             list_of_dirs.append(path)
     latest_dir = max(list_of_dirs, key=lambda p: p.stat().st_ctime)
 
-    return latest_dir / "wandb-events.jsonl"
+    return latest_dir, latest_dir / "wandb-events.jsonl"
 
 
 def load(directory_name):
-    path = find_latest(directory_name)
+    directory, path = find_latest(directory_name)
     output_dic = {}
 
     clock = 0
     while not path.exists():
         clock += 1
         time.sleep(1)
-        print(clock)
         if clock >= 60:
             raise FileExistsError(f"{path} is not found!")
 
@@ -36,11 +35,11 @@ def load(directory_name):
         for key in item.keys():
             output_dic.setdefault(key, []).append(item[key])
 
-    return pd.DataFrame.from_dict(output_dic)
+    return pd.DataFrame.from_dict(output_dic), directory
 
 
 def plot(directory_name):
-    output = load(directory_name)
+    output, directory = load(directory_name)
     timestamp = output["_runtime"] / 60
 
     cpu_utilization = output.get("system.cpu", [0] * len(timestamp))
@@ -61,53 +60,88 @@ def plot(directory_name):
     gpu_memory_alloc = output.get("system.gpu.0.memoryAllocated", [0] * len(timestamp))
     gpu_temp = output.get("system.gpu.0.temp", [0] * len(timestamp))
 
-    fig, axs = plt.subplots(2, 2)
-    axs[0, 0].plot(timestamp, cpu_utilization)
-    axs[0, 0].set_title("CPU Utilization (%)")
-    axs[0, 1].plot(timestamp, disk_utilization)
-    axs[0, 1].set_title("Disk I/O \n Utilization (%)")
-    axs[1, 0].plot(timestamp, proc_cpu_threads)
-    axs[1, 0].set_title("Process CPU \n Threads In Use")
-    axs[1, 1].plot(timestamp, network_sent, ls="--")
-    axs[1, 1].plot(timestamp, network_recv)
-    axs[1, 1].set_title("Network Traffic (bytes)")
-    plt.tight_layout(h_pad=1.6)
+    plt.plot(timestamp, cpu_utilization)
+    plt.title("CPU Utilization (%)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "CPU_Utilization.png")
+    plt.clf()
 
-    fig1, axs1 = plt.subplots(2, 2)
-    axs1[0, 0].plot(timestamp, memory)
-    axs1[0, 0].set_title("System Memory Utilization (%)")
-    axs1[0, 1].plot(timestamp, proc_memory_availableMB)
-    axs1[0, 1].set_title("Process Memory \n Available (non-swap) (MB)")
-    axs1[1, 0].plot(timestamp, proc_memory_rssMB)
-    axs1[1, 0].set_title("Process Memory \n In Use (non-swap) (MB)")
-    axs1[1, 1].plot(timestamp, proc_memory_percent)
-    axs1[1, 1].set_title("Process Memory \n In Use (non-swap) (%)")
+    plt.plot(timestamp, disk_utilization)
+    plt.title("Disk I/O Utilization (%)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Disk_IO_Utilization.png")
+    plt.clf()
 
-    plt.tight_layout(h_pad=1.6)
+    plt.plot(timestamp, proc_cpu_threads)
+    plt.title("Process CPU Threads In Use")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "CPU_Threads.png")
+    plt.clf()
+
+    plt.plot(timestamp, network_sent, ls="--")
+    plt.plot(timestamp, network_recv)
+    plt.title("Network Traffic (bytes)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Network_Traffic.png")
+    plt.clf()
+
+    plt.plot(timestamp, memory)
+    plt.title("System Memory Utilization (%)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Memory_Utilization.png")
+    plt.clf()
+
+    plt.plot(timestamp, proc_memory_availableMB)
+    plt.title("Process Memory Available (non-swap) (MB)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Proc_Memory_available.png")
+    plt.clf()
+
+    plt.plot(timestamp, proc_memory_rssMB)
+    plt.title("Process Memory In Use (non-swap) (MB)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Proc_Memory_MB.png")
+    plt.clf()
+
+    plt.plot(timestamp, proc_memory_percent)
+    plt.title("Process Memory \n In Use (non-swap) (%)")
+    plt.xlabel("Time(minutes)")
+    plt.grid()
+    plt.savefig(directory / "Proc_Memory_Percent.png")
+    plt.clf()
 
     if "system.gpu.0.gpu" in output:
-        fig2, axs2 = plt.subplots(2, 2)
-        axs2[0, 0].plot(timestamp, gpu_utilization)
-        axs2[0, 0].set_title("GPU Utilization (%)")
-        axs2[0, 1].plot(timestamp, gpu_memory)
-        axs2[0, 1].set_title("GPU Memory Allocated (%)")
-        axs2[1, 0].plot(timestamp, gpu_memory_alloc)
-        axs2[1, 0].set_title("GPU Time Spent \n Accessing Memory (%)")
-        axs2[1, 1].plot(timestamp, gpu_temp)
-        axs2[1, 1].set_title("GPU Temp (℃)")
+        plt.plot(timestamp, gpu_utilization)
+        plt.title("GPU Utilization (%)")
+        plt.xlabel("Time(minutes)")
+        plt.grid()
+        plt.savefig(directory / "GPU_Utilization.png")
+        plt.clf()
 
-        plt.tight_layout(h_pad=1.6)
+        plt.plot(timestamp, gpu_memory)
+        plt.title("GPU Memory Allocated (%)")
+        plt.xlabel("Time(minutes)")
+        plt.grid()
+        plt.savefig(directory / "GPU_Memory_Allocated.png")
+        plt.clf()
 
-        for ax2 in axs2.flat:
-            ax2.set(xlabel="Time(minutes)")
-            ax2.grid()
+        plt.plot(timestamp, gpu_memory_alloc)
+        plt.title("GPU Time Spent Accessing Memory (%)")
+        plt.xlabel("Time(minutes)")
+        plt.grid()
+        plt.savefig(directory / "GPU_Memory_Time.png")
+        plt.clf()
 
-    for ax in axs.flat:
-        ax.set(xlabel="Time(minutes)")
-        ax.grid()
-
-    for ax1 in axs1.flat:
-        ax1.set(xlabel="Time(minutes)")
-        ax1.grid()
-
-    plt.show()
+        plt.plot(timestamp, gpu_temp)
+        plt.title("GPU Temp (℃)")
+        plt.xlabel("Time(minutes)")
+        plt.grid()
+        plt.savefig(directory / "GPU_Temp.png")
+        plt.clf()
