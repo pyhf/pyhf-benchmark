d9ef1bb44ad7605b95e031f9f554131e10758bd6
Bo Zheng <cool_zb@outlook.com>
2020-07-10 17:40:40 -0500

    Package project

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyhf-benchmark-BoZheng",
+    version="0.0.1",
+    author="Bo Zheng",
+    author_email="bozheng96@gmail.com",
+    description="pyhf Benchmark Suite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyhf/pyhf-benchmark",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
