e0796b0ce8cd1ee76f94a070c7e0be51c5768847
Bo Zheng <cool_zb@outlook.com>
2020-07-22 11:59:14 -0500

    Add library and CI skeleton

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 import setuptools
 
+extras_require = {
+    "develop": [
+        "check-manifest",
+        "pytest~=5.2",
+        "pytest-cov~=2.8",
+        "pytest-console-scripts~=0.2",
+        "bumpversion~=0.5",
+        "pyflakes",
+        "pre-commit",
+        "black",
+        "twine",
+    ],
+}
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyhf-benchmark-BoZheng",
+    name="pyhf_benchmark",
     version="0.0.1",
     author="Bo Zheng",
     author_email="bozheng96@gmail.com",
@@ -19,4 +34,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    extras_require=extras_require,
+    entry_points={
+        "console_scripts": ["pyhf_benchmark=pyhf_benchmark.commandline:pyhf_benchmark"]
+    },
 )
