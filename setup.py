b8ccfad7e87d04c165e6858eded021b0d75be242
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-28 20:10:15 -0500

    Try multiple extras

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 import setuptools
 
 extras_require = {
-    "develop": [
-        "check-manifest",
-        "pytest~=5.2",
-        "pytest-cov~=2.8",
-        "pytest-console-scripts~=0.2",
-        "bumpversion~=0.5",
-        "pyflakes",
-        "pre-commit",
-        "black",
-        "twine",
-        "pyhf[backends]~=0.5",
-    ],
+    "tensorflow": ["pyhf[tensorflow]"],
+    "torch": ["pyhf[torch]"],
+    "jax": ["pyhf[jax]"],
+    "minuit": ["pyhf[minuit]"],
+    "backends": ["pyhf[backends]"],
 }
+extras_require["lint"] = sorted(set(["pyflakes", "black"]))
+extras_require["develop"] = sorted(
+    set(
+        extras_require["backends"]
+        + extras_require["lint"]
+        + [
+            "check-manifest",
+            "pytest~=5.2",
+            "pytest-cov~=2.8",
+            "pytest-console-scripts~=0.2",
+            "bumpversion~=0.5",
+            "pre-commit",
+            "twine",
+        ]
+    )
+)
 extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
 setuptools.setup(
