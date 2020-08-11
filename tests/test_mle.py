b1a864f71c5c184cc8ab6f21a0542682067db1ae
Bo Zheng <cool_zb@outlook.com>
2020-08-11 11:31:53 -0500

    Modify mle jsonfile tests

--- tests/test_mle.py
+++ tests/test_mle.py
@@ -1,5 +1,6 @@
 import json
 import builtins
+import shutil
 import pyhf_benchmark.mle as mle
 from pathlib import Path
 
@@ -7,6 +8,8 @@ from pathlib import Path
 def test_get_bkg_and_signal(mocker):
 
     directory = Path("directory_path")
+    if not directory.exists():
+        directory.mkdir(parents=True, exist_ok=True)
     model_point = None
     mocker.patch("pathlib.Path.glob", return_value=[Path("file.json")])
     mocker.patch("builtins.open")
@@ -15,3 +18,10 @@ def test_get_bkg_and_signal(mocker):
     assert Path.glob.called
     assert builtins.open.called
     assert json.load.called
+
+    file_name = Path("BkgOnly.json")
+    file = (directory / file_name).open("a+")
+    _, signal = mle.get_bkg_and_signal(directory, model_point)
+    file.close()
+    shutil.rmtree(directory)
+    assert not signal
